3b853496135813946905093ef2e9e33eede0c00e
Mirza Ukasyah <mirzauksyh@gmail.com>
2023-03-25 13:44:30 +0700

    Use CreateAndroidComposeRule and get node text using string res

--- app/src/androidTest/java/com/mirz/mycomposetesting/ui/CalculatorAppTest.kt
+++ app/src/androidTest/java/com/mirz/mycomposetesting/ui/CalculatorAppTest.kt
@@ -1,21 +1,25 @@
 package com.mirz.mycomposetesting.ui
 
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import com.mirz.mycomposetesting.ui.theme.MyComposeTestingTheme
+import com.mirz.mycomposetesting.R
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class CalculatorAppTest{
+class CalculatorAppTest {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
     @Before
     fun setUp() {
         composeTestRule.setContent {
-            MyComposeTestingTheme() {
+            MyComposeTestingTheme {
                 CalculatorApp()
 
             }
@@ -24,9 +28,26 @@ class CalculatorAppTest{
 
     @Test
     fun calculate_area_of_rectangle_correct() {
-        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
-        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
-        composeTestRule.onNodeWithText("Hitung!").performClick()
-        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("3")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, "12.0"))
+            .assertExists()
+    }
+
+
+    @Test
+    fun wrong_input_not_calculated() {
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("..3")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, 0.0))
+            .assertExists()
     }
 }
